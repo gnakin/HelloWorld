5ae32c31a4db01c4eea7c92d1f2372894806a8f8
gnakin <bojesejar2@gmail.com>
2023-08-21 19:11:31 +0530

    updated hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true){
+            System.out.println("Do you want to say it?");
+            String input = sc.nextLine();
+            if(input.equals("y")){
+                System.out.println("Hello world!");
+            } else {
+                break;
+            }
+        }
+
     }
 }
